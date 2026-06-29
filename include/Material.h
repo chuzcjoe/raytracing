28589b6ecd6f2415d0c06ac4fd7931a5f07c4425
chuzcjoe <joechu0510@gmail.com>
2026-06-29 09:18:17 -0700

    update

--- include/Material.h
+++ include/Material.h
@@ -15,7 +15,7 @@ class Lambertian : public Material {
  public:
   Lambertian(const Color& albedo) : albedo_(albedo) {}
 
-  bool Scatter(const Ray&, const HitRecord& rec, Color& attenuation,
+  bool Scatter([[maybe_unused]] const Ray& ray_in, const HitRecord& rec, Color& attenuation,
                Ray& ray_scattered) const override {
     auto scatter_direction = rec.normal + random_unit_vector();
 
@@ -55,19 +55,19 @@ class Dielectric : public Material {
  public:
   Dielectric(double refraction_index) : refraction_index_(refraction_index) {}
 
-  bool Scatter(const Ray& r_in, const HitRecord& rec, Color& attenuation,
+  bool Scatter(const Ray& ray_in, const HitRecord& rec, Color& attenuation,
                Ray& ray_scattered) const override {
     attenuation = Color(1.0, 1.0, 1.0);
     double ri = rec.front_face ? (1.0 / refraction_index_) : refraction_index_;
 
-    Vec3 unit_direction = unit_vector(r_in.direction());
+    Vec3 unit_direction = unit_vector(ray_in.direction());
     double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
     double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
     bool cannot_refract = ri * sin_theta > 1.0;
     Vec3 direction;
 
-    if (cannot_refract)
+    if (cannot_refract || Reflectance(cos_theta, ri) > random_double())
       direction = reflect(unit_direction, rec.normal);
     else
       direction = refract(unit_direction, rec.normal, ri);
@@ -80,4 +80,11 @@ class Dielectric : public Material {
   // Refractive index in vacuum or air, or the ratio of the material's refractive index over
   // the refractive index of the enclosing media
   double refraction_index_;
+
+  static double Reflectance(double cosine, double refraction_index) {
+    // Use Schlick's approximation for reflectance.
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+  }
 };
